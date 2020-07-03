924114e16e8cb7d08038f9e31f440c0c7d2c6f67
Allana Anderson <allana.anderson17@gmail.com>
2020-07-03 16:55:01 -0600

    changed input tage to text area tags. Rearranged the class assignments on html rows and took away future class designation. Finalized if/else statements that determine color of the time blocks. Created save button click functionality that sends user input to internal storage. Each row pulls from local storage for its text content.

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 
-
-//! Trying to use moment.js. Not going well
+//Sets date at the top of the page
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
 
+function updateHour(){
+    var currentHour = moment().hours();
+    console.log(currentHour);
+    $(".time-block").each(function(){
+        var blockTime = $(this).attr("id");
+        console.log(blockTime);
+
+        if(currentHour > blockTime){
+            $(this).addClass("past");
+        } else if(currentHour == blockTime){
+            // $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }else{
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+}
+
+updateHour();
+
+//get input from user. Store it in an object. Print to the screen
+$(".saveBtn").on("click", function(){
+    var todoInput= $(this).siblings(".description").val().trim();
+    var rowTime = $(this).parent().attr("id");
+
+    window.localStorage.setItem(rowTime, todoInput);
+})
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
-//? How do you get the time? Need to display that. 
-//?How do I get font awesome to work
-//if the time is equal to or later than current block, change color
-// Save to local storage on button click
-//Print to-do on text input area. Maybe blank preview-tag?
+
